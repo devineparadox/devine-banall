7199ce7afd58bbfe9b045516366fdb065a1e841b
Devine <devineparadoxz@gmail.com>
2024-07-10 22:34:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-# config.py
+import os
 
-OWNER_ID = 6440363814
-API_ID = 29230755
-API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
-BOT_TOKEN = "7411280606:AAGfj62QYs1GGAiL6w-w3_OcByR7XAjOSo8"
-BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ᏼᴀɴᴀʟʟ"
-BOT_USERNAME = "DevineBanall_bot"
+OWNER_ID = int(os.getenv('OWNER_ID', ''))
+API_ID = int(os.getenv('API_ID', ''))
+API_HASH = os.getenv('API_HASH', '')
+
+BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+BOT_NAME = os.getenv('BOT_NAME', '')
+BOT_USERNAME = os.getenv('BOT_USERNAME', '')
+
 START_IMG = "https://telegra.ph//file/a166560a9c505a1fbe92e.jpg"
 MUSIC = "https://t.me/devineXmusic_bot"
 SOURCE = "https://github.com/devineparadox/Devine-Banall"
