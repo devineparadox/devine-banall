1702048825b600319990100db3c163a432f2a32e
Devine <devineparadoxz@gmail.com>
2024-07-10 23:31:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 
-OWNER_ID = 6440363814
-API_ID = 29230755
-API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
 
-BOT_TOKEN = "7290224551:AAGROA8Jv8amXKfOzJUxwsXGfAGXYVt_z1Q"
+# Get this value from my.telegram.org/apps
+API_ID = 29230755
+API_HASH = ""
+
+# Get this value on Telegram by /id
+OWNER_ID = 6440363814
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = ""
 BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ᏼᴀɴᴀʟʟ"
 BOT_USERNAME = "DevineBanall_bot"
 
+# Optional 
 START_IMG = "https://telegra.ph//file/a166560a9c505a1fbe92e.jpg"
 MUSIC = "https://t.me/devineXmusic_bot"
 SOURCE = "https://github.com/devineparadox/Devine-Banall"
