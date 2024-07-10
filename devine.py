eccef72b560383ba1ec6a6a4f082a4e33f2c814c
Devine <devineparadoxz@gmail.com>
2024-07-10 19:04:54 +0530

    Update devine.py

--- devine.py
+++ devine.py
@@ -4,10 +4,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 START_IMG = "https://telegra.ph//file/721a7c1b34195a7a6f727.jpg"
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+devine = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Handler for /start command
-@app.on_message(filters.command("start") & filters.private)
+@devine.on_message(filters.command("start") & filters.private)
 async def start_command(client, message: Message):
     await message.reply_photo(
         photo=START_IMG,
@@ -27,18 +27,18 @@ async def start_command(client, message: Message):
     )
 
 # Handler for /banall command
-@app.on_message(filters.command("banall") & filters.group)
+@devine.on_message(filters.command("banall") & filters.group)
 async def ban_all(client, message: Message):
     if message.from_user.id == OWNER_ID:
         chat_id = message.chat.id
         banned_count = 0
         failed_count = 0
-        async for member in client.get_chat_members(chat_id):
+        async for member in devine.iter_chat_members(chat_id):
             if member.user.id != OWNER_ID and not member.user.is_bot:
                 try:
-                    await client.kick_chat_member(chat_id, member.user.id)
+                    await devine.kick_chat_member(chat_id, member.user.id)
                     user_mention = f"@{member.user.username}" if member.user.username else member.user.first_name
-                    await client.send_message(chat_id, f"{user_mention} has been banned.", reply_to_message_id=message.message_id)
+                    await devine.send_message(chat_id, f"{user_mention} has been banned.", reply_to_message_id=message.message_id)
                     banned_count += 1
                 except Exception as e:
                     failed_count += 1
@@ -51,4 +51,4 @@ async def ban_all(client, message: Message):
     else:
         await message.reply_text("You are not authorized to use this command.")
 
-app.run()
+devine.run()
