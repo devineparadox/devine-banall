df1564dd86ac95e714a6565b22df8a8204c8e340
Devine <devineparadoxz@gmail.com>
2024-07-10 19:22:58 +0530

    Update devine.py

--- devine.py
+++ devine.py
@@ -1,54 +1,60 @@
-from config import OWNER_ID, API_ID, API_HASH, BOT_TOKEN
-from pyrogram import Client, filters
+from os import getenv
+import logging
+from pyrogram import Client, filters, idle
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import ChatAdminRequired
 
-START_IMG = "https://telegra.ph//file/721a7c1b34195a7a6f727.jpg"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-devine = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# config vars
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER = getenv("OWNER")
 
-# Handler for /start command
-@devine.on_message(filters.command("start") & filters.private)
+# pyrogram client
+app = Client(
+    "banall",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+@app.on_message(filters.command("start") & filters.private)
 async def start_command(client, message: Message):
     await message.reply_photo(
-        photo=START_IMG,
-        caption="Hello! I am a bot designed to ban all members from a group. "
-                "Use /banall to ban all members in a group where I am admin.",
+        photo="https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
+        caption="ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜɪɴ ᴀ ғᴇᴡ sᴇᴄᴏɴᴅs!\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Creator", url=f"tg://openmessage?user_id={OWNER_ID}"),
-                    InlineKeyboardButton("Updates", url="https://t.me/devine_network")
+                    InlineKeyboardButton("ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
                 ],
                 [
-                    InlineKeyboardButton("Source", url="https://github.com/devineparadox/Devine-banall"),
+                    InlineKeyboardButton("Update Channel", url="https://t.me/devine_network"),
+                    InlineKeyboardButton("Source Code", url="https://github.com/devineparadox/Devine-banall")
                 ]
             ]
         )
     )
 
-# Handler for /banall command
-@devine.on_message(filters.command("banall") & filters.group)
-async def ban_all(client, message: Message):
-    if message.from_user.id == OWNER_ID:
-        chat_id = message.chat.id
-        banned_count = 0
-        failed_count = 0
-        async for member in devine.get_chat_members(chat_id):
-            if member.user.id != OWNER_ID and not member.user.is_bot:
-                try:
-                    await devine.kick_chat_member(chat_id, member.user.id)
-                    user_mention = f"@{member.user.username}" if member.user.username else member.user.first_name
-                    await devine.send_message(chat_id, f"{user_mention} has been banned.", reply_to_message_id=message.message_id)
-                    banned_count += 1
-                except Exception as e:
-                    failed_count += 1
-                    await message.reply_text(f"Failed to ban {user_mention}: {e}")
-        
-        if failed_count == 0:
-            await message.reply_text("All members have been banned.")
-        else:
-            await message.reply_text(f"Banned {banned_count} members. {failed_count} members could not be banned.")
-    else:
-        await message.reply_text("You are not authorized to use this command.")
+@app.on_message(filters.command("banall") & filters.group)
+async def banall_command(client, message: Message):
+    print("getting members from {}".format(message.chat.id))
+    async for i in app.iter_chat_members(message.chat.id):
+        try:
+            await app.ban_chat_member(chat_id=message.chat.id, user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("failed to kick {} from {}".format(i.user.id, e))
 
-devine.run()
+    print("process completed")
+
+# start bot client
+app.start()
+print("Banall-Bot Booted Successfully")
+idle()
