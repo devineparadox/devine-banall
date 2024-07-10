550dbed403e5e808f2520c30805b1a6cf5963552
Devine <devineparadoxz@gmail.com>
2024-07-10 21:43:41 +0530

    Update devine.py

--- devine.py
+++ devine.py
@@ -3,7 +3,7 @@ import logging
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import ChatAdminRequired
-from config import API_HASH, API_ID, BOT_TOKEN, OWNER_ID, START_IMG, UPDATE_CHANNEL, SOURCE, MUSIC
+from config import API_HASH, API_ID, BOT_TOKEN, OWNER_ID, START_IMG, UPDATE_CHANNEL, SOURCE, MUSIC, BOT_NAME, BOT_USERNAME
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -23,7 +23,7 @@ devine = Client(
 async def start_command(client, message: Message):
     await message.reply_photo(
         photo=START_IMG,
-        caption=f"ʏᴏᴏ {message.from_user.mention} ✨\n\nɪ'ᴍ {devine.mention}\n\nᴀ ᴘʏʀᴏɢʀᴀᴍ-ʙᴀsᴇᴅ ʙᴏᴛ ᴘʀᴏɢʀᴀᴍᴍᴇᴅ ᴛᴏ ʙᴀɴ ᴏʀ ᴡɪᴘᴇ ᴏᴜᴛ ᴀʟʟ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ɪɴ ᴊᴜsᴛ ᴀ ғᴇᴡ sᴇᴄᴏɴᴅs.\n──────────────────\nɢʀᴀɴᴛ ᴍᴇ ᴜɴʀᴇsᴛʀɪᴄᴛᴇᴅ ᴀᴄᴄᴇss ᴛᴏ ᴛᴇsᴛ ᴍʏ ᴄᴀᴘᴀʙɪʟɪᴛɪᴇs.",
+        caption=f"ʏᴏᴏ {message.from_user.mention} ✨\n\nɪ'ᴍ [{BOT_NAME}](https://t.me/{BOT_USERNAME}?start)\n\nᴀ ᴘʏʀᴏɢʀᴀᴍ-ʙᴀsᴇᴅ ʙᴏᴛ ᴘʀᴏɢʀᴀᴍᴍᴇᴅ ᴛᴏ ʙᴀɴ ᴏʀ ᴡɪᴘᴇ ᴏᴜᴛ ᴀʟʟ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ɪɴ ᴊᴜsᴛ ᴀ ғᴇᴡ sᴇᴄᴏɴᴅs.\n──────────────────\nɢʀᴀɴᴛ ᴍᴇ ᴜɴʀᴇsᴛʀɪᴄᴛᴇᴅ ᴀᴄᴄᴇss ᴛᴏ ᴛᴇsᴛ ᴍʏ ᴄᴀᴘᴀʙɪʟɪᴛɪᴇs.",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
