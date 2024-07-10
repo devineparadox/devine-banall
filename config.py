938116b6da0a1c7c0812be676c54a5c3b0ebb4ea
Devine <devineparadoxz@gmail.com>
2024-07-10 21:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ OWNER_ID = 6440363814
 API_ID = 29230755
 API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
 BOT_TOKEN = "7411280606:AAGfj62QYs1GGAiL6w-w3_OcByR7XAjOSo8"
+
+MUSIC = "https://t.me/devineXmusic_bot?start"
+SOURCE = "https://github.com/devineparadox/Devine-Banall"
+UPDATE_CHANNEL = "https://t.me/Devine_Network"
