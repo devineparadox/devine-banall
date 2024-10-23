186abe068b7cd4e45c4a5fa9e0c0d786b9f35019
Devine <devineparadoxz@gmail.com>
2024-10-23 20:48:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
-# Get this value from my.telegram.org/apps
 API_ID = 29230755
 API_HASH = ""
 
-# Get this value on Telegram by /id
 OWNER_ID = 6440363814
 
-# Get your token from @BotFather on Telegram.
 BOT_TOKEN = "7289814789:AAHycs8aDrJHS68xC2ZepcCWmKoR_akfVEc"
 BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ᏼᴀɴᴀʟʟ"
 BOT_USERNAME = "DevineBanall_bot"
 
-# Optional 
-START_IMG = "https://telegra.ph//file/e45175489f16c43a28e34.jpg"
-MUSIC = "https://t.me/devineXmusic_bot"
 SOURCE = "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x64\x65\x76\x69\x6e\x65\x70\x61\x72\x61\x64\x6f\x78\x2f\x64\x65\x76\x69\x6e\x65\x2d\x62\x61\x6e\x61\x6c\x6c"
 UPDATE_CHANNEL = "https://t.me/Devine_Network"
